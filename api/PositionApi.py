ab283104d936d0c1fa9bdcd8107a47127f8785cb
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-06-03 18:11:23 +0200

    Task-3: created PositionApi.py, and added it to main.py

---
+++ api/PositionApi.py
@@ -0,0 +1,64 @@
+import json
+from flask import Blueprint, request
+from dao import PositionDao
+from model.MySqlResponse import MySqlResponse
+from model.Position import Position
+
+position_api = Blueprint("position_api", __name__)
+dao = PositionDao
+
+
+@position_api.route("/position", methods=["POST"])
+def create_position():
+    data = request.get_json()
+    name = data.get("name")
+    responsible_id = data.get("responsible_id")
+    position = Position(name=name)
+    response = dao.create_position(position, responsible_id)
+    return json.dumps(response.__dict__)
+
+
+@position_api.route("/positions", methods=["GET"])
+def get_positions():
+    response = dao.fetch_positions()
+    if response.response_code == MySqlResponse.OK:
+        positions = response.response
+        response_data = {
+            "response": [position.__dict__ for position in positions],
+            "response_code": response.response_code
+        }
+        return json.dumps(response_data)
+    else:
+        return json.dumps(response.__dict__)
+
+
+@position_api.route("/position/<int:position_id>", methods=["GET"])
+def get_position_by_id(position_id):
+    response = dao.get_position_by_id(position_id)
+    if response.response_code == MySqlResponse.OK:
+        position = response.response
+        response_data = {
+            "response": position.__dict__,
+            "response_code": response.response_code
+        }
+        return json.dumps(response_data)
+    else:
+        return json.dumps(response.__dict__)
+
+
+@position_api.route("/position/<int:position_id>", methods=["PUT"])
+def update_position(position_id):
+    data = request.get_json()
+    name = data.get("name")
+    responsible_id = data.get("responsible_id")
+    position = Position(position_id=position_id, name=name)
+    response = dao.update_position(position, responsible_id)
+    return json.dumps(response.__dict__)
+
+
+@position_api.route("/position/<int:position_id>", methods=["DELETE"])
+def delete_position(position_id):
+    data = request.get_json()
+    responsible_id = data.get("responsible_id")
+    response = dao.delete_position(position_id, responsible_id)
+    return json.dumps(response.__dict__)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api.EmployeeApi import employee_api
+from api.PositionApi import position_api
 
 app = Flask(__name__)
 app.register_blueprint(employee_api)
+app.register_blueprint(position_api)
 
 if __name__ == '__main__':
     print('Hey from Task tracking server')
