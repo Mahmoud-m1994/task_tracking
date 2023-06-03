71ff942c9c6d464b8120256226dec989ad571783
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-06-03 22:12:01 +0200

    added employee_position_api to app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from api.EmployeeApi import employee_api
 from api.PositionApi import position_api
+from api.EmployeePositionApi import employee_position_api
 
 app = Flask(__name__)
 app.register_blueprint(employee_api)
 app.register_blueprint(position_api)
+app.register_blueprint(employee_position_api)
 
 if __name__ == '__main__':
     print('Hey from Task tracking server')
