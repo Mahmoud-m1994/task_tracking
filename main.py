7dd33524c2d389592f62f03cf4c7418c3f9de058
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-06-03 10:40:38 +0200

    Task-2: created simple server using Flask, and added employee_api to it using blueprint

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from database.DatabaseConnector import connect_to_mysql
+from flask import Flask
+from api.EmployeeApi import employee_api
+
+app = Flask(__name__)
+app.register_blueprint(employee_api)
 
 if __name__ == '__main__':
     print('Hey from Task tracking server')
-    connect_to_mysql()
+    # connect_to_mysql()
+    app.run()
