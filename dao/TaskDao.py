5348904fbacfa1ba9ac256433d361edc49014930
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-06-04 17:45:16 +0200

    Added unit tests for Task and connecting tasks to employees

--- dao/TaskDao.py
+++ dao/TaskDao.py
@@ -125,10 +125,17 @@ def update_task(task: Task, responsible_id: str) -> MySqlResponse:
         disconnect_from_mysql(connection)
 
 
-def assign_task(task_id: int, assigned_to_id: str, assigned_by_id: str,
-                assigned_date: datetime = None) -> MySqlResponse:
+def assign_task(
+        task_id: int,
+        assigned_to_id: str,
+        assigned_by_id: str,
+        assigned_date: datetime = None
+) -> MySqlResponse:
     assigned_by_has_active_position = employee_has_active_position(assigned_by_id)
     assigned_to_has_active_position = employee_has_active_position(assigned_to_id)
+
+    if not task_exists(task_id):
+        return MySqlResponse("Task not found", MySqlResponse.NOT_FOUND)
 
     if not assigned_by_has_active_position or not assigned_to_has_active_position:
         return MySqlResponse("Both assigned_by and assigned_to must have an active position",
@@ -167,6 +174,9 @@ def assign_task(task_id: int, assigned_to_id: str, assigned_by_id: str,
 
 
 def unassigned_task(task_id: int, unassigned_from: str, unassigned_by: str) -> MySqlResponse:
+    if not task_exists(task_id):
+        return MySqlResponse("Task not found", MySqlResponse.NOT_FOUND)
+
     if not employee_has_active_position(unassigned_by):
         return MySqlResponse("Unassigned requires an active position for unassigned_by",
                              response_code=MySqlResponse.UNAUTHORIZED)
@@ -183,6 +193,7 @@ def unassigned_task(task_id: int, unassigned_from: str, unassigned_by: str) -> M
         connection.commit()
 
         if cursor.rowcount > 0:
+            print("unassigned_task  2")
             return MySqlResponse("Task unassigned successfully", response_code=MySqlResponse.OK)
         else:
             return MySqlResponse("Task assignment failed. Task not found or assignment conditions not met",
@@ -235,6 +246,48 @@ def delete_all_employee_tasks():
     finally:
         cursor.close()
         disconnect_from_mysql(connection)
+
+
+def get_max_task_id() -> int:
+    try:
+        connection = connect_to_mysql()
+        cursor = connection.cursor()
+
+        query = "SELECT MAX(task_id) FROM Task"
+        cursor.execute(query)
+        result = cursor.fetchone()[0]
+
+        cursor.close()
+        connection.close()
+
+        if result is not None:
+            return int(result)
+        else:
+            return -1
+    except Exception as error:
+        print("Error while connecting to MySQL:", error)
+        return -1
+
+
+def task_exists(task_id: int) -> bool:
+    try:
+        connection = connect_to_mysql()
+        cursor = connection.cursor()
+
+        query = "SELECT task_id FROM Task WHERE task_id = %s"
+        cursor.execute(query, (task_id,))
+        result = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        if result is not None:
+            return True
+        else:
+            return False
+    except Exception as error:
+        print("Error while connecting to MySQL:", error)
+        return False
 
 
 delete_all_employee_tasks.__unittest_skip__ = True
---
+++ tests/EmployeeTask.py
@@ -0,0 +1,152 @@
+import unittest
+from datetime import datetime, timedelta
+from unittest.mock import patch
+
+from dao.EmployeePositionDao import create_employee_position, delete_all_employee_positions
+from dao.TaskDao import get_max_task_id
+from model.EmployeePosition import EmployeePosition
+from dao import TaskDao
+from model.MySqlResponse import MySqlResponse
+from model.Task import Task
+
+dao = TaskDao
+
+
+class EmployeeTaskDaoTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        employee_position = EmployeePosition(
+            1,
+            "2",
+            1,
+            datetime(2025, 1, 1),
+            datetime(2028, 1, 1),
+            datetime.now(),
+            1
+        )
+        create_employee_position(employee_position=employee_position, responsible_id="1")
+
+    def test_1_create_task_success(self):
+        updated_or_assigned_by = "2"
+        task = Task(
+            task_id=1,
+            name="Task 1",
+            description="Description 1",
+            date_active=datetime.now(),
+            created_at=datetime.now(),
+            status="Active"
+        )
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.create_task(task, updated_or_assigned_by)
+        self.assertEqual(response.response_code, MySqlResponse.CREATED)
+        self.assertEqual(response.response, "Task created successfully")
+
+    def test_2_create_task_unauthorized(self):
+        updated_or_assigned_by = "0"
+        task = Task(
+            task_id=1,
+            name="Task 1",
+            description="Description 1",
+            date_active=datetime.now(),
+            created_at=datetime.now(),
+            status="Active"
+        )
+        with patch("dao.Authorization.employee_has_active_position", return_value=False):
+            response = dao.create_task(task, updated_or_assigned_by)
+        self.assertEqual(response.response_code, MySqlResponse.UNAUTHORIZED)
+        self.assertEqual(response.response, "Only users with an active position can create tasks")
+
+    def test_3_fetch_tasks_success(self):
+        response = dao.fetch_tasks()
+        self.assertEqual(response.response_code, MySqlResponse.OK)
+        self.assertTrue(isinstance(response.response, list))
+
+    def test_4_get_task_by_id_success(self):
+        task_id = get_max_task_id()
+        response = dao.get_task_by_id(task_id)
+        self.assertEqual(response.response_code, MySqlResponse.OK)
+        self.assertTrue(isinstance(response.response, Task))
+
+    def test_5_get_task_by_id_not_found(self):
+        task_id = 100
+        response = dao.get_task_by_id(task_id)
+        self.assertEqual(response.response_code, MySqlResponse.NOT_FOUND)
+        self.assertEqual(response.response, "Task not found")
+
+    def test_6_update_task_success(self):
+        updated_or_assigned_by = "2"
+        task = Task(
+            task_id=get_max_task_id() or -1,
+            name="Updated Task",
+            description="Updated Description",
+            date_active=datetime.now() + timedelta(days=365 * 4),
+            created_at=datetime.now(),
+            status="Updated Status"
+        )
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.update_task(task, updated_or_assigned_by)
+        self.assertEqual(response.response_code, MySqlResponse.OK)
+        self.assertEqual(response.response, "Task updated successfully")
+
+    def test_7_update_task_not_found(self):
+        updated_or_assigned_by = "2"
+        task = Task(
+            task_id=100,
+            name="Updated Task",
+            description="Updated Description",
+            date_active=datetime.now(),
+            created_at=datetime.now(),
+            status="Updated Status"
+        )
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.update_task(task, updated_or_assigned_by)
+        self.assertEqual(response.response_code, MySqlResponse.NOT_FOUND)
+        self.assertEqual(response.response, "Task not found")
+
+    def test_8_assign_task_success(self):
+        task_id = get_max_task_id() or -1
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.assign_task(
+                task_id=task_id,
+                assigned_to_id="2",
+                assigned_by_id="2",
+                assigned_date=datetime.now()
+            )
+        self.assertEqual(response.response_code, MySqlResponse.CREATED)
+        self.assertEqual(response.response, "Task assigned successfully")
+
+    def test_9_assign_task_not_found(self):
+        task_id = 10234
+        updated_or_assigned_by = "2"
+        assigned_by_id = "2"
+        assigned_date = datetime.now()
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.assign_task(task_id, updated_or_assigned_by, assigned_by_id, assigned_date)
+        self.assertEqual(response.response_code, MySqlResponse.NOT_FOUND)
+        self.assertEqual(response.response, "Task not found")
+
+    def test_10_unassigned_task_not_found(self):
+        task_id = 100
+        unassigned_from = "2"
+        unassigned_by = "2"
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.unassigned_task(task_id, unassigned_from, unassigned_by)
+        self.assertEqual(response.response_code, MySqlResponse.NOT_FOUND)
+        self.assertEqual(response.response, "Task not found")
+
+    def test_11_delete_task_not_found(self):
+        task_id = 100
+        updated_or_assigned_by = "2"
+        with patch("dao.Authorization.employee_has_active_position", return_value=True):
+            response = dao.delete_task(task_id, updated_or_assigned_by)
+        self.assertEqual(response.response_code, MySqlResponse.NOT_FOUND)
+        self.assertEqual(response.response, "Task not found")
+
+    @classmethod
+    def tearDownClass(cls):
+        delete_all_employee_positions()
+        dao.delete_all_employee_tasks()
+
+
+if __name__ == '__main__':
+    unittest.main()
