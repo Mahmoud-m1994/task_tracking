d2442f38b2b50fbf5378469a42b82cd4022585ee
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-06-04 15:10:22 +0200

    fix PositionDaoTest.py also due to renaming IsAdmin file to Authorization

--- tests/PositionDaoTest.py
+++ tests/PositionDaoTest.py
@@ -10,7 +10,7 @@ class PositionDaoTest(unittest.TestCase):
     def test_1_create_position_success(self):
         position = Position("0203", "UNIT TEST POSITION")
         responsible_id = "1"
-        with patch("dao.IsEmployeeAdmin.is_admin", return_value=True):
+        with patch("dao.Authorization.is_admin", return_value=True):
             response = create_position(position, responsible_id)
         self.assertEqual(response.response_code, MySqlResponse.CREATED)
         self.assertEqual(response.response, "Position created successfully")
@@ -18,7 +18,7 @@ class PositionDaoTest(unittest.TestCase):
     def test_2_create_position_unauthorized(self):
         position = Position("0203", "UNIT TEST POSITION")
         responsible_id = "0"
-        with patch("dao.IsEmployeeAdmin.is_admin", return_value=False):
+        with patch("dao.Authorization.is_admin", return_value=False):
             response = create_position(position, responsible_id)
         self.assertEqual(response.response_code, MySqlResponse.UNAUTHORIZED)
         self.assertEqual(response.response, "Only admins can create positions")
@@ -26,7 +26,7 @@ class PositionDaoTest(unittest.TestCase):
     def test_3_create_position_already_existing(self):
         position = Position("0203", "UNIT TEST POSITION")
         responsible_id = "1"
-        with patch("dao.IsEmployeeAdmin.is_admin", return_value=True):
+        with patch("dao.Authorization.is_admin", return_value=True):
             response = create_position(position, responsible_id)
         self.assertEqual(response.response_code, MySqlResponse.ALREADY_EXISTING)
         self.assertEqual(response.response, "Position with the same name or position_id already exists")
@@ -46,7 +46,7 @@ class PositionDaoTest(unittest.TestCase):
     def test_6_delete_position_not_authorized(self):
         position = Position("0203", "UNIT TEST POSITION")
         responsible_id = "0"
-        with patch("dao.IsEmployeeAdmin.is_admin", return_value=False):
+        with patch("dao.Authorization.is_admin", return_value=False):
             response = delete_position(position, responsible_id)
         self.assertEqual(response.response_code, MySqlResponse.UNAUTHORIZED)
         self.assertEqual(response.response, "Only admins can delete positions")
@@ -54,7 +54,7 @@ class PositionDaoTest(unittest.TestCase):
     def test_7_delete_position_success(self):
         position = Position("0203", "UNIT TEST POSITION")
         responsible_id = "1"
-        with patch("dao.IsEmployeeAdmin.is_admin", return_value=True):
+        with patch("dao.Authorization.is_admin", return_value=True):
             response = delete_position(position, responsible_id)
         self.assertEqual(response.response_code, MySqlResponse.OK)
         self.assertEqual(response.response, "Position deleted successfully")
